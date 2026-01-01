71eb8f9965527e2b4fc9b6e9cdd5625a41f97138
Kaustube <kaustubhs1706@gmail.com>
2026-01-02 01:08:34 +0530

    Update script.js

--- forntend/script.js
+++ forntend/script.js
@@ -1,15 +1,1 @@
-trackArea.addEventListener("mouseleave", () => {
-    if (mouseData.length < 2) return;
 
-    // Save as JSON string
-    const dataStr = JSON.stringify(mouseData);
-    console.log("Data JSON:", dataStr);
-
-    // Optional: let user download the data
-    const blob = new Blob([dataStr], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "mouse_data.json";
-    a.click();
-});
